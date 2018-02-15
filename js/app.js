4c4a3636bfef527b58f3274313451b02e5dccaa5
Jimmy Guzman <jimmy.guzman@outlook.com>
2018-02-15 02:22:41 -0600

    added del, clear and equals functions

--- js/app.js
+++ js/app.js
@@ -5,11 +5,37 @@ function handleDisplay(key) {
     displayForm.entries.value += key;
   }
 }
+function deleteEntry() {
+  let entries = Array.from(displayForm.entries.value);
+  entries.pop();
+  let newEntry = entries.join("");
+  displayForm.reset();
+  handleDisplay(newEntry);
+}
 
 function evaluate() {
-    let answer = eval(displayForm.entries.value);
-    displayForm.reset();
-    handleDisplay(answer);
+  let answer = eval(displayForm.entries.value);
+  console.log(answer);
+  displayForm.reset();
+  handleDisplay(answer);
+}
+
+function fix(value) {
+  if (value % 1 != 0) {
+    let rounded = Math.round(value);
+    let length = String(rounded).length;
+    return 9 - length;
+  } else {
+    return 0;
+  }
+}
+
+function sqrt() {
+  let answer = eval(displayForm.entries.value);
+  answer = Math.sqrt(answer);
+  console.log(answer);
+  displayForm.reset();
+  handleDisplay(answer.toFixed(fix(answer)));
 }
 
 function handleNumKey(e) {
@@ -17,6 +43,10 @@ function handleNumKey(e) {
     displayForm.reset();
   } else if (String(this.dataset.key) === "=") {
     evaluate();
+  } else if (String(this.dataset.key) === "del") {
+    deleteEntry();
+  } else if (String(this.dataset.key) === "sqrt") {
+    sqrt();
   } else {
     handleDisplay(this.dataset.key);
   }
